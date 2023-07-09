f8cbfe2bbb747e7dc5173c56ba6b9479cdcc69f1
ZU <zshaihova@gmail.com>
2023-07-09 19:00:08 +0300

    ДЗ выполнено

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,11 @@ public class Main {
         List<String> strings1 = new ArrayList<>(List.of("яблоко", "груша", "вишня", "яблоко", "банан", "банан"));
         List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
         task1(nums);
+        System.out.println();
         task2(nums);
+        System.out.println();
         task3(strings1);
+        System.out.println();
         task4(strings);
     }
 
@@ -17,34 +20,34 @@ public class Main {
                 System.out.printf("%s ", i);
             }
         }
-        System.out.println();
     }
 
     public static void task2(List<Integer> nums) {
-        Collections.sort(nums);
+//        Collections.sort(nums);
         Set<Integer> setNums = new HashSet<>(nums);
         for (Integer i : nums) {
             if (i % 2 != 0) {
                 setNums.remove(i);
             }
         }
-        System.out.printf("%s ", setNums);
-        System.out.println();
+        Set<Integer> setNums2 = new TreeSet<>(setNums);
+        System.out.printf("%s ", setNums2);
     }
 
     public static void task3(List<String> strings1) {
         Set<String> strings2 = new HashSet<>(strings1);
         System.out.printf("%s ", strings2);
-        System.out.println();
+
     }
 
     public static void task4(List<String> strings) {
-        Map<String, Integer> mapSrtings = new HashMap<>();
+        Map<Integer, String> mapSrtings = new HashMap<>();
         for (int i = 0; i < strings.size(); i++) {
             int copy = Collections.frequency(strings, strings.get(i));
-            mapSrtings.put(strings.get(i), copy);
+            mapSrtings.put(copy,strings.get(i));
         }
-           System.out.println(mapSrtings.toString());
+        Map<Integer, String> mapSrtings2 = new  TreeMap<>(mapSrtings);
+        System.out.println(mapSrtings2.toString());
     }
 }
 
